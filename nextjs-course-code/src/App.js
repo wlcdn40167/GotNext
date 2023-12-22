28bff0a584011505c7ea65799a4f2c8b41f464e7
wlcdn40167 <wlcdn40167@gmail.com>
2023-12-22 23:19:45 +0800

    day 2

---
+++ nextjs-course-code/src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <div>
+        <h2>Title</h2>
+        <div>
+          <button>Delete</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
