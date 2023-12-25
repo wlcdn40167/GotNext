49b8f297c02ce8db299204630959a90bef52824f
wlcdn40167 <wlcdn40167@gmail.com>
2023-12-25 22:56:13 +0800

    day 4

--- react-crash-course/src/components/PostsList.jsx
+++ react-crash-course/src/components/PostsList.jsx
@@ -1,12 +1,45 @@
+import { setState } from "react";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 import NewPost from "./NewPost";
+import { useState } from "react";
+import Modal from "./Modal";
+
 function PostsList() {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+  function authorChangeHandle(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
+  let modalContent;
+
+  if (modalIsVisible) {
+    modalContent = (
+      <Modal onClose={hideModalHandler}>
+        <NewPost
+          onBodyChange={bodyChangeHandler}
+          onAuthorChange={authorChangeHandle}
+        />
+      </Modal>
+    );
+  }
+
   return (
     <>
-      <NewPost />
+      {modalContent}
+
       <ul className={classes.posts}>
-        <Post author="Jade" body="React.js is very nice" />
+        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Jade" body="React.js is very nice" />
       </ul>
     </>
