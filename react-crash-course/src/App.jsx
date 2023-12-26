4b67c701412802ca81a298fe5b2fbb2a68ae18a0
wlcdn40167 <wlcdn40167@gmail.com>
2023-12-26 23:26:31 +0800

    day 5

--- react-crash-course/src/App.jsx
+++ react-crash-course/src/App.jsx
@@ -1,15 +1,15 @@
-import PostsList from "./components/PostsList";
 import { useState } from "react";
+import PostsList from "./components/PostsList";
 import MainHeader from "./components/MainHeader";
 function App() {
   const [modalIsVisible, setModalIsVisible] = useState(false);
 
-  function hideModalHandler() {
-    setModalIsVisible(false);
-  }
-
   function showModalHandler() {
     setModalIsVisible(true);
+  }
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
   }
 
   return (
